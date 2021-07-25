c1dbcc28e258ae6b7f5e969dc20a0d6718357928
Hugo Borges <hugo.uchoas@hydrasystems.com.br>
2021-07-25 20:40:57 -0300

    [EDX-4513] Base enemy animator interaction

--- 11. Final/edx_final/Assets/MyAssets/Scripts/Enemy/EnemyController.cs
+++ 11. Final/edx_final/Assets/MyAssets/Scripts/Enemy/EnemyController.cs
@@ -1,5 +1,6 @@
 ﻿using behaviors;
 using behaviors.shooting;
+using core;
 using helpers;
 using System;
 using UnityEngine;
@@ -7,12 +8,14 @@ using util;
 
 namespace enemy
 {
-    [RequireComponent(typeof(SpriteRenderer))]
-    [RequireComponent(typeof(Collider2D))]
     public class EnemyController : SpaceshipController, IPoolable
     {
 
         public event Action<EnemyController> onDestroy = delegate { };
+
+        // Collision handling
+        [SerializeField] private GameObject _colliderHolder;
+        private Action _onCollisionEnter2D;
 
         // ========================== Init ============================
 
@@ -25,11 +28,42 @@ namespace enemy
             mShooting.InstallWeapon(ShootingManager.WeaponPosition.LEFT);
             //mShooting.InstallWeapon(ShootingManager.WeaponPosition.CENTER);
             mShooting.InstallWeapon(ShootingManager.WeaponPosition.RIGHT);
+
+            // Add the collider listener to the child object with the Collider2D
+            EnemyCollider collider = _colliderHolder.AddComponent<EnemyCollider>();
+            collider.onCollisionEnter2D = OnCollisionEnter2D;
+
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
             onDestroy(this);
+        }
+
+
+        // ========================== Animator methods ============================
+
+        public void Animator_Shoot()
+        {
+            Debug.Log("Shoot");
+            mShooting.Fire();
+        }
+    }
+
+
+    // ----------------------------------------------------------------------------------
+    // ========================== External collider handling ============================
+    // ----------------------------------------------------------------------------------
+
+
+    [RequireComponent(typeof(Collider2D))]
+    public class EnemyCollider: MonoBehaviour
+    {
+        public Action<Collision2D> onCollisionEnter2D;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            onCollisionEnter2D(collision);
         }
     }
 }
